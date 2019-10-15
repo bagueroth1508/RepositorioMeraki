06776c717597af774f80f7ee3a3470033a3f87cb
diego <dcahuayo@meraki-s.com>
2019-10-15 11:28:00 -0500

    Applicativo Meraki

---
+++ APPMERAKI2/src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,30 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent {
+
+  mobileQuery: MediaQueryList;
+
+  items:string[]=['Bienvenidos','Empezar'];
+  shouldRun =true;
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+  isAuthenticated(){
+
+  }
+}
+  
